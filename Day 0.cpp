eb6af4c1c077923e732bd141c6280aefcbaf6636
Sinthia Rupa <85312355+Sinthia666@users.noreply.github.com>
2021-06-12 09:20:11 +0600

    Add files via upload

---
+++ Day 0.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   string s; // declare a variable to hold our input
+   getline(cin, s); // get a line of input from cin and save it to our variable
+  
+   // Your first line of output goes here
+   cout << "Hello, World." << endl;
+
+
+   // Write the second line of output
+    cout<< s<<endl;
+   return 0;
+}
